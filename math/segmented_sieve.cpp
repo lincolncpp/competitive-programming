a4d20a2309862e87817b0c6420dc7d599ab04005
lincolncpp <lincolncpp@usp.br>
2019-11-22 20:16:26 -0300

    segmented prime sieve

---
+++ math/segmented_sieve.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+vector<ll> sieve(int n){
+    vector<bool>is_composite(n+1, false);
+    vector<ll>primes;
+
+    for(int i = 2;i <= n;i++){
+        if (!is_composite[i]) primes.push_back(i);
+
+        for(auto p:primes){
+            if (i*p > n) break;
+            is_composite[i*p] = true;
+            if (i % p == 0) break;
+        }
+    }
+
+    return primes;
+}
+
+vector<bool> ranged_sieve(ll l, ll r){
+    vector<ll>primes = sieve((int)sqrt(r)+1);
+    vector<bool>mark(r-l+1, true);
+
+    for(auto p:primes){
+        ll a = (l/p)*p;
+        if (a < l || a == p) a += p;
+        for(;a <= r;a += p) mark[a-l] = false;
+    }
+
+    return mark;
+}
+
+int main(){
+
+    ll l = 1000000000000;
+    ll r = 1000000000000+500;
+
+    cout << "Primes between " << l << " and " << r << endl;
+    vector<bool>mark = ranged_sieve(l, r);
+    for(int i = 0;i < (int)mark.size();i++){
+        if (mark[i]) cout << l+i << endl;
+    }
+
+    return 0;
+}
