8ed54beb406831cd1731c5ceaf776c06f491d10b
lincolncpp <lincolncpp@usp.br>
2019-11-22 16:41:57 -0300

    added string algorithms

---
+++ string/prefix_function.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> prefix(const string &s){
+    int n = (int)s.size();
+    vector<int>p(n);
+
+    for(int i = 1;i < n;i++){
+        p[i] = p[i-1];
+        while(p[i] > 0 && s[p[i]] != s[i]) p[i] = p[p[i]-1];
+        if (s[p[i]] == s[i]) p[i]++;
+    }
+
+    return p;
+}
+
+int main(){
+
+    vector<int>p = prefix("aabxaabxay");
+
+    for(int i = 0;i < p.size();i++) cout << p[i] << " ";
+    cout << endl;
+
+    return 0;
+}
---
+++ string/z.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int>z(const string &s){
+    int n = (int)s.size();
+    vector<int>v(n);
+
+    int l, r;
+    l = r = 0;
+    for(int i = 1;i < n;i++){
+        if (i <= r) v[i] = min(v[i-l], r-i+1);
+        else while(i+v[i] < n && s[i+v[i]] == s[v[i]]) v[i]++;
+        if (i > r){
+            l = i;
+            r = i+v[i]-1;
+        }
+    }
+
+    return v;
+}
+
+int main(){
+
+    vector<int>v = z("aabxaabxay");
+
+    for(int i = 0;i < v.size();i++) cout << v[i] << " ";
+    cout << endl;
+
+    return 0;
+}
