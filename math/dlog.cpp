b3362fa888f9bd24ac7bfdea127a1778440a562e
lincolncpp <lincolncpp@usp.br>
2019-12-03 19:31:38 -0300

    discrete log

--- math/dlog.cpp
+++ math/dlog.cpp
@@ -4,32 +4,28 @@ using namespace std;
 
 #define ll long long
 
-ll mul(ll a, ll b, ll MOD){
-    return (__int128(a)*b)%MOD;
-}
+int dlog(int a, int b, int m){
+    int n = (int)sqrt(m)+1;
 
-ll dlog(ll a, ll b, ll m){
-    ll n = sqrt(m)+1;
-
-    ll an = 1;
+    int an = 1;
     for(int i = 0;i < n;i++){
-        an = mul(an, a, m);
+        an = (an*(ll)a)%m;
     }
 
-    unordered_map<ll, ll>values;
-    for(ll p = 1, cur = an;p <= n;p++){
+    unordered_map<int, int>values;
+    for(int p = 1, cur = an;p <= n;p++){
         if (cur == 0) break;
         if (values.count(cur) == 0) values[cur] = p;
-        cur = mul(cur, an, m);
+        cur = (an*(ll)an)%m;
     }
 
-    ll c = b;
-    for(ll q = 0;q <= n;q++){
+    int c = b;
+    for(int q = 0;q <= n;q++){
         if (values.count(c)){
-            ll x = values[c]*n-q;
+            int x = values[c]*n-q;
             return x;
         }
-        c = mul(c, a, m);
+        c = (c*(ll)a)%m;
     }
 
     return -1;
@@ -37,7 +33,7 @@ ll dlog(ll a, ll b, ll m){
 
 int main(){
 
-    cout << dlog(5, 33, 58) << endl;
+    cout << dlog(3, 4, 11) << endl;
 
     return 0;
 }
