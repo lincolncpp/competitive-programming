dbf0896a1108aece53e8923b96d0340e9cafacbf
lincolncpp <lincolncpp@usp.br>
2020-02-25 18:13:42 -0300

    code update

--- data_structure/segtree.cpp
+++ data_structure/segtree.cpp
@@ -2,69 +2,57 @@
 
 using namespace std;
 
-template<typename T, int MAXN>
-struct segtree{
-    vector<T> tree;
-    vector<T> lazy;
+const int maxn = 1e5;
+int tree[4*maxn+7] = {};
+int lazy[4*maxn+7] = {};
+int n;
 
-    segtree(){
-        tree.resize(4*MAXN+1);
-        lazy.resize(4*MAXN+1);
+void build(const vector<int>&v, int node = 1, int tl = 0, int tr = n-1){
+    if (tl == tr) return void(tree[node] = v[tl]);
+    int mid = (tl+tr)/2;
+    build(v, node*2, tl, mid);
+    build(v, node*2+1, mid+1, tr);
+    tree[node] = tree[node*2]+tree[node*2+1];
+}
+
+void push(int node, int tl, int tr){
+    if (tl == tr) tree[node] += lazy[node];
+    else{
+        tree[node] += lazy[node]*(tr-tl+1);
+        lazy[node*2] += lazy[node];
+        lazy[node*2+1] += lazy[node];
     }
+    lazy[node] = 0;
+}
 
-    void build(const vector<T> &arr, int v = 1, int tl = 0, int tr = MAXN-1){
-        if (tr == tl) tree[v] = arr[tl];
-        else{
-            int mid = (tl+tr)/2;
-            build(arr, 2*v, tl, mid);
-            build(arr, 2*v+1, mid+1, tr);
-            tree[v] = tree[2*v]+tree[2*v+1];
-        }
+int update(int l, int r, int val, int node = 1, int tl = 0, int tr = n-1){
+    push(node, tl, tr);
+    if (l > r) return 0;
+    if (tl == l && tr == r){
+        lazy[node] += val;
+        push(node, tl, tr);
+        return tree[node];
     }
+    int mid = (tl+tr)/2;
+    int left = update(l, min(r, mid), val, node*2, tl, mid);
+    int right = update(max(mid+1, l), r, val, node*2+1, mid+1, tr);
+    tree[node] = tree[node*2]+tree[node*2+1];
+    return left+right;
+}
 
-    void push(int v, int tl, int tr){
-        tree[v] += (tr-tl+1)*lazy[v];
-        if (tr != tl) lazy[2*v] = lazy[2*v+1] = lazy[v];
-        lazy[v] = 0;
-    }
-
-    T update(int l, int r, T value, int v = 1, int tl = 0, int tr = MAXN-1){
-        if (l > r) return 0;
-        if (tl == l && tr == r) {
-            lazy[v] += value;
-            push(v, tl, tr);
-            return tree[v];
-        }
-
-        push(v, tl, tr);
-        int mid = (tl+tr)/2;
-
-        T query = 0;
-        query += update(l, min(r, mid), value, 2*v, tl, mid);
-        query += update(max(l, mid+1), r, value, 2*v+1, mid+1, tr);
-        tree[v] = tree[2*v]+tree[2*v+1];
-
-        return query;
-    }
-
-    T query(int l, int r){return update(l, r, 0);}
-};
+int query(int l, int r){
+    return update(l, r, 0);
+}
 
 int main(){
 
-    vector<int>v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vector<int>v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    n = v.size();
 
-    segtree<int, 10> tree;
-    tree.build(v);
+    build(v);
 
-    tree.update(0, 9, 99);
-    tree.update(0, 9, 7);
-    tree.update(0, 9, -7);
-    tree.update(0, 9, -99);
-
-    tree.update(9, 9, 90);
-
-    cout << tree.query(0, 9) << endl;
+    update(0, n-1, 10);
+    cout << query(0, n-1) << endl;
 
     return 0;
 }
