e14a96b172a588e6dd81ed84f7ad185256113e3a
Lincoln <lincolncpp@usp.br>
2020-03-20 16:37:58 -0300

    code improvement

--- string/hashing.cpp
+++ string/hashing.cpp
@@ -5,9 +5,9 @@ using namespace std;
 #define ll long long
 #define hash apdmfoiwahofqjenfoj
 
-const int maxn = 1e5;
-const int p = 31;
-const int mod = 1e9+7;
+const int maxn = 1e6;
+const ll p = 31;
+const ll mod = 1e9+7;
 
 ll power[maxn+7] = {};
 ll inverse[maxn+7] = {};
@@ -20,16 +20,25 @@ ll modpow(ll x, ll n){
 }
 
 struct hash{
+    int n;
     ll val = 0;
+    ll rval = 0;
     vector<ll>prefix;
+    vector<ll>suffix;
 
     hash(const string &s){
-        int n = (int)s.size();
+        n = (int)s.size();
         prefix.resize(n);
+        suffix.resize(n);
 
         for(int i = 0;i < n;i++){
             (val += s[i]*power[i]) %= mod;
             prefix[i] = val;
+        }
+
+        for(int i = n-1;i >= 0;i--){
+            (rval += s[i]*power[n-1-i]) %= mod;
+            suffix[i] = rval;
         }
     }
 
@@ -37,14 +46,30 @@ struct hash{
         if (i == 0) return prefix[j];
         return (prefix[j]-prefix[i-1]+mod) % mod *inverse[i] % mod;
     }
+
+    ll rsubstr(int i, int j){
+        if (j == n-1) return suffix[i];
+        return (suffix[i]-suffix[j+1]+mod) % mod *inverse[n-1-j] % mod;
+    }
+
+    bool is_palindrome(int i, int j){
+        if (i == j) return true;
+        int len = j-i+1;
+        int k = len/2;
+        return substr(i, i+k-1) == rsubstr(j-k+1, j);
+    }
 };
 
 void build(){
+    ll ip = modpow(p, mod-2);
+    
     ll pwr = 1;
+    ll ipwr = 1;
     for(int i = 0;i < maxn;i++){
         power[i] = pwr;
-        inverse[i] = modpow(power[i], mod-2);
+        inverse[i] = ipwr;
         (pwr *= p) %= mod;
+        (ipwr *= ip) %= mod;
     }
 }
 
@@ -52,9 +77,11 @@ int main(){
 
     build();
 
-    hash h("abacadaabacadaabacadaabacadaabacadaabacadaabacada");
-    cout << h.val << endl;
-    cout << h.substr(13, 21) << endl;
+    string s = "abacaba";
+    int n = (int)s.size();
+
+    hash h(s);
+    cout << h.is_palindrome(0, n-1) << endl;
 
     return 0;
 }
