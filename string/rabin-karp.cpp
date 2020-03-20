1dedf05de6926e18f8a4cf1ae84a081f85a45b7a
Lincoln <lincolncpp@usp.br>
2020-03-20 19:48:45 -0300

    rabin-karp

---
+++ string/rabin-karp.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+// s = text
+// t = pattern
+vector<int> rabin_karp(const string &s, const string &t){
+    const int p = 53;
+    const int mod = 1e9+7;
+    int n = (int)s.size();
+    int m = (int)t.size();
+
+    vector<ll>power(max(n, m));
+    ll pwr = 1;
+    for(int i = 0;i < max(n, m);i++){
+        power[i] = pwr;
+        (pwr *= p) %= mod;
+    }
+
+    vector<ll>prefix(n+1, 0);
+    for(int i = 1;i <= n;i++)
+        prefix[i] = (prefix[i-1] + (s[i-1]*power[i-1]) % mod) % mod;
+
+    ll hash_t = 0;
+    for(int i = 0;i < m;i++)
+        (hash_t += s[i]*power[i] % mod) %= mod;
+
+    vector<int>res;
+    for(int i = 0;i+m-1 < n;i++){
+        ll a = ((prefix[i+m]-prefix[i]+mod)%mod);
+        ll b = (hash_t*power[i]) % mod;
+
+        if (a == b) res.push_back(i);
+    }
+    return res;
+}
+
+int main(){
+
+    vector<int>occurences = rabin_karp("abababab", "ab");
+    for(int i:occurences) cout << i << endl;
+
+    return 0;
+}
