5220f6da55096d07bdba845893886f68dff790c0
Fernando Lincoln <lincolncpp@usp.br>
2021-03-03 12:59:13 -0300

    Update DSU

--- data_structure/dsu.cpp
+++ data_structure/dsu.cpp
@@ -2,31 +2,30 @@
 
 using namespace std;
 
-#define N 100000
-
-int link[N];
-int siz[N];
+const int maxn = 1e5;
+int link[maxn];
+int siz[maxn];
 
 int find(int x){
-    while(x != link[x]) x = link[x];
+    while(x != link[x]){
+        link[x] = link[link[x]];
+        x = link[x];
+    }
     return x;
 }
 
 void unite(int a, int b){
     a = find(a);
     b = find(b);
+    if (a == b) return;
     if (siz[a] < siz[b]) swap(a, b);
     link[b] = a;
     siz[a] += siz[b];
 }
 
-bool same(int a, int b){
-    return find(a) == find(b);
-}
-
 int main(){
 
-    for(int i = 0;i < N;i++){
+    for(int i = 0;i < maxn;i++){
         link[i] = i;
         siz[i] = 1;
     }
