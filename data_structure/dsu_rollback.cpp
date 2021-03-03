a4a8e265e61500c60d84d313a994c00c0459db45
Fernando Lincoln <lincolncpp@usp.br>
2021-03-03 13:01:14 -0300

    Update DSU rollback

--- data_structure/dsu_rollback.cpp
+++ data_structure/dsu_rollback.cpp
@@ -7,13 +7,13 @@ const int maxn = 2e5+123;
 struct operation{
     int a, b;
     int rnka, rnkb;
-    operation(int a, int b, int rnka, int rnkb) : a(a), b(b), rnka(rnka), rnkb(rnkb) {}
+    operation(int a_, int b_, int rnka_, int rnkb_) : a(a_), b(b_), rnka(rnka_), rnkb(rnkb_) {}
 };
 
 int link[maxn];
 int rnk[maxn];
 int comp = 0;
-stack<operation>history;
+stack<operation>hist;
 
 int find(int x){
     while(x != link[x]) x = link[x];
@@ -25,21 +25,21 @@ void unite(int a, int b){
     b = find(b);
     if (a == b) return;
     if (rnk[b] > rnk[a]) swap(a, b);
-    history.push(operation(a, b, rnk[a], rnk[b]));
+    hist.push(operation(a, b, rnk[a], rnk[b]));
     comp--;
     link[b] = a;
     if (rnk[a] == rnk[b]) rnk[a]++;
 }
 
 void rollback(){
-    if (history.empty()) return;
+    if (hist.empty()) return;
 
-    operation op = history.top();
-    history.pop();
+    operation op = hist.top();
+    hist.pop();
 
     link[op.a] = op.a;
-    rnk[op.a] = op.rnka;
     link[op.b] = op.b;
+    rnk[op.a] = op.rnka;
     rnk[op.b] = op.rnkb;
     comp++;
 }
@@ -49,18 +49,18 @@ int main(){
     int n = 10;
     comp = n;
 	for(int i = 1;i <= n;i++) link[i] = i;
-    
+
     unite(1, 2);
     cout << comp << endl;
     unite(3, 4);
     cout << comp << endl;
     unite(1, 3);
     cout << comp << endl;
-    
+
     rollback();
     rollback();
     rollback();
-    
+
     cout << comp << endl;
 
     return 0;
